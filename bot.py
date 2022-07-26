c5ad9b0998324de672e87e62d03e6015184f1679
Artyom.Yuryshev <yuryshev6613@gmail.com>
2022-07-26 20:19:54 +0300

    feat: implement reminder feature

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+import config
 import telebot
+import time
+from datetime import datetime
 
-bot = telebot.TeleBot("")
+# bot token
+bot = telebot.TeleBot(config.TOKEN)
 
-# start action
+# start command
 @bot.message_handler(commands=["start"])
 def start(message):
     HelloMessage = f"Gamarjoba, <b>{message.from_user.first_name} {message.from_user.last_name}</b>! \nЯ вам тут напоминать буду."
@@ -14,13 +18,24 @@ def get_user_text(message):
     ReminderMessage = "/start \n/help \n/reminder \n/poll"
     bot.send_message(message.chat.id, ReminderMessage, parse_mode='html')
 
-# reminder func
+# reminder command
 @bot.message_handler(commands=["reminder"])
 def get_user_text(message):
-    ReminderMessage = "Окай, я буду напомню вам о необходимости зарегистрироваться каждый понедельник в 11:58."
-    bot.send_message(message.chat.id, ReminderMessage, parse_mode="html")
+    ReminderHint = "Окай, я буду напомню вам o необходимости зарегистрироваться каждый понедельник в 11:58."
+    bot.send_message(message.chat.id, ReminderHint, parse_mode="html")
+    Reminder = "@snickerzzz регай!"
+    while(True):
+        CurrentDay = datetime.today().isoweekday()
+        CurrentTime = datetime.now().strftime("%H:%M:%S")
+        TimeStamp = str(CurrentDay) + "-" + CurrentTime
+        print(TimeStamp)
+        time.sleep(1)
+        if TimeStamp == "1-11:59:00":
+            print(TimeStamp)
+            bot.send_message(message.chat.id, Reminder)
 
-# poll func
+
+# poll command
 @bot.message_handler(commands=["poll"])
 def get_user_text(message):
     ReminderMessage = "Окай, я буду создавать опрос каждую субботу в 11:58."
