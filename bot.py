a7129683c6b4175ed473006534f86479b6a6b002
Artyom.Yuryshev <yuryshev6613@gmail.com>
2022-07-26 20:26:49 +0300

    refactor: remove start command

--- bot.py
+++ bot.py
@@ -6,16 +6,10 @@ from datetime import datetime
 # bot token
 bot = telebot.TeleBot(config.TOKEN)
 
-# start command
-@bot.message_handler(commands=["start"])
-def start(message):
-    HelloMessage = f"Gamarjoba, <b>{message.from_user.first_name} {message.from_user.last_name}</b>! \nЯ вам тут напоминать буду."
-    bot.send_message(message.chat.id, HelloMessage, parse_mode="html")
-
 # help command
 @bot.message_handler(commands=["help"])
 def get_user_text(message):
-    ReminderMessage = "/start \n/help \n/reminder \n/poll"
+    ReminderMessage = "/help \n/reminder \n/poll"
     bot.send_message(message.chat.id, ReminderMessage, parse_mode='html')
 
 # reminder command
